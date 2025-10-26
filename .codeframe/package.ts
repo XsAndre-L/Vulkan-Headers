95b6f4f446672bb8b6e10d05c3c7f5fb2c2d9c11
xsandre-l <ahloubser12@gmail.com>
2025-10-26 13:42:55 +0200

    .

---
+++ .codeframe/package.ts
@@ -0,0 +1,31 @@
+import {
+  BuildType,
+  HeaderList,
+  OUTPUT_DIR,
+} from "../../../../src/types/package-config.ts";
+import { runPackageAction } from "../../../../src/packages.ts";
+
+import { resolve } from "node:path";
+import { argv } from "node:process";
+
+export const build = (cwd: string = process.cwd()): BuildType => {
+  const INSTALL_DIR = resolve(cwd, "../", OUTPUT_DIR);
+
+  const VULKAN_INSTALL = resolve(INSTALL_DIR, "vulkan");
+
+  // Shipped Backends
+
+  const vulkan: HeaderList = {
+    type: "headers",
+    libs: {
+      [VULKAN_INSTALL]: ["include"],
+    },
+  };
+
+  return vulkan satisfies BuildType;
+};
+
+const args = argv.slice(2);
+const [action = "help"] = args;
+
+await runPackageAction(action, process.cwd(), build());
